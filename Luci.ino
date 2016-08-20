1cc44fffa0f27f1ec0aec3c45ce4bfec1bd448af
Matteo Bonora <bonora.matteo@gmail.com>
2016-08-20 11:13:05 +0200

    Add code
    Add old, undocumented code

---
+++ Luci.ino
@@ -0,0 +1,105 @@
+#define pinc A0
+
+int pina = 2, pinb = 3, pind = 4, pine = 5, l1 = 8, l2 = 9, l3 = 10, lpi = 11, lpt = 12, pinf = 13, cron = 600, croff = 560, exton = 640, extoff = 620, intervalcr = 30000;
+bool a, b, c = false, d, e, f = false, wait1 = false, wait2 = false;
+unsigned long waituntil1, waituntil2;
+
+void setup() {
+        pinMode(a, INPUT);
+        pinMode(b, INPUT);
+        pinMode(d, INPUT);
+        pinMode(e, INPUT);
+        pinMode(pinc, INPUT);
+
+        pinMode(l1, OUTPUT);
+        pinMode(l2, OUTPUT);
+        pinMode(l3, OUTPUT);
+        pinMode(lpi, OUTPUT);
+        pinMode(lpt, OUTPUT);
+        pinMode(pinf, OUTPUT);
+
+        Serial.begin(9600);
+}
+
+void loop() {
+
+        a = digitalRead(pina);
+        b = digitalRead(pinb);
+        d = digitalRead(pind);
+        e = digitalRead(pine);
+
+        if(Serial)
+                Serial.println(analogRead(pinc));
+
+
+
+        if (analogRead(pinc) <= croff) {
+
+                if (wait1 == false) {
+                        waituntil1 = millis() + intervalcr;
+                        wait1=true;
+                } else if (millis() >= waituntil1 && wait1) {
+                        c = false;
+                        wait1=false;
+                }
+
+        } else if (analogRead(pinc) >= cron) {
+
+                if (wait1 == false) {
+                        waituntil1 = millis() + intervalcr;
+                        wait1=true;
+                } else if (millis() >= waituntil1 && wait1) {
+                        c = true;
+                        wait1=false;
+                }
+        }
+
+
+
+
+        if (analogRead(pinc) <= extoff) {
+
+                if (wait2 == false) {
+                        waituntil2 = millis() + 2000;
+                        wait2=true;
+                } else if (millis() >= waituntil2 && wait2) {
+                        f = false;
+                        wait2=false;
+                }
+
+        } else if (analogRead(pinc) >= exton) {
+
+                if (wait2 == false) {
+                        waituntil2 = millis() + 2000;
+                        wait2=true;
+                } else if (millis() >= waituntil2 && wait2) {
+                        f = true;
+                        wait2=false;
+                }
+        }
+
+
+        if (!a || d || (e && !b) || (b && c)) {
+                digitalWrite(l1, HIGH);
+        } else { digitalWrite(l1, LOW); }
+
+        if (!a || (a && !b && (d || e)) || a && b && c) {
+                digitalWrite(l2, HIGH);
+        } else { digitalWrite(l2, LOW); }
+
+        if (b && (!a || c)) {
+                digitalWrite(l3, HIGH);
+        } else { digitalWrite(l3, LOW); }
+
+        if (a && !(b && c)) {
+                digitalWrite(lpi, HIGH);
+        } else { digitalWrite(lpi, LOW); }
+
+        if (a && !b) {
+                digitalWrite(lpt, HIGH);
+        } else { digitalWrite(lpt, LOW); }
+
+        if (a && b && f) {
+                digitalWrite(pinf, HIGH);
+        } else { digitalWrite(pinf, LOW); }
+}
